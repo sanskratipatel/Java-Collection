09ae3631394304bf01f493dcc9b9587d1fe23f71
Sanskrati Patel <patelsanskrati@gmail.com>
2024-12-09 14:30:38 +0530

    Add Arraylist

--- AArraylistProgram.java
+++ AArraylistProgram.java
@@ -1,3 +1,26 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+
 public class AArraylistProgram {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> li = new ArrayList<>();
+        li.add(1);
+        li.add(2);
+        li.add(3);
+        li.add(4);
+        System.out.println("Get = " +li.get(3));
+        System.out.println("Size = "+li.size());
+        for(int x: li){
+            System.out.println(x);
+        }
+        System.out.println("Contain method = " +li.contains(2));
+        System.out.println("Remove method = "+ li.remove(2));
+        for(int a :li) {
+            System.out.println(a);
+        }
+    }
+
 
 }
---
+++ BLinkedList.java
@@ -0,0 +1,5 @@
+public class BLinkedList {
+    public static void main(String[] args) {
+
+    }
+}
