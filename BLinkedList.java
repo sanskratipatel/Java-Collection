8cdeaf3fbab91a539b08a77bfa05e618a2b8a2a5
Sanskrati Patel <patelsanskrati@gmail.com>
2024-12-09 22:34:11 +0530

    Add code in Linkedlist

--- BLinkedList.java
+++ BLinkedList.java
@@ -1,5 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+
 public class BLinkedList {
     public static void main(String[] args) {
+        LinkedList<String> str = new LinkedList<>();
+        str.add("Apple");
+        str.add("Banana");
+        str.add("Papaya");
+        str.add("Orange");
 
+        System.out.println("Getting str from Index = " +str.get(2));
+        for(String x : str){
+            System.out.println(x);
+        }
+        System.out.println("Size of Linked List = " + str.size());
+        System.out.println("STR Contain banana or not = "+ str.contains("Banana"));
+        System.out.println("STR Contain Grapes or not = "+ str.contains("Grapes"));
     }
 }
